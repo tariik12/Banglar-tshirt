6dd02e78f4e1cf70905decaf9366c2e959782ca7
tarikul-islam <tariikul12@gmail.com>
2023-04-07 00:35:55 +0600

    T Shirt & cart component done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import TShirt from '../Tshirt/Tshirt';
+import Cart from '../Cart/Cart';
 
 const Home = () => {
     const tshirts = useLoaderData()
     console.log(tshirts)
 
     return (
-        <div>
-            <h1>This is Home :{tshirts.length}</h1>
+        <div className='home-container'>
+        <div className='t-shirts-container'>
+        {
+            tshirts.map(tshirt =><TShirt
+                
+                key={_id}
+                tshirt ={tshirt}
+                />)
+          }
+        </div>
+        <div className='cart-container'>
+          <Cart />
+        </div>
         </div>
     );
 };
---
+++ src/components/Tshirt/TShirt.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TShirt = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TShirt;
